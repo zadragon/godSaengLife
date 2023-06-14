e7b0ec7660ec335570c34fa544d365a256ea07c7
choijihye1017 <chlwi6752@naver.com>
2023-06-14 23:30:11 +0900

    홈화면 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,10 @@ function Home() {
                 로그인
             </button>
             <div className="bg-blue-500"> Tailwind Css 적용 테스트 </div>
+            <div className="bg-blue-500">
+                <div>식단 사진첩</div>
+                <div>전체보기&nbsp;＞</div>
+            </div>
         </div>
     );
 }
