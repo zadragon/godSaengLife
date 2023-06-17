10456780bb2b85e872e3af251ce64dec91984894
zadragon <mase0804@naver.com>
2023-06-17 16:30:02 +0900

    로그아웃/리드미 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { AuthApi } from '../shared/api';
+import { useCookies } from 'react-cookie';
 
 const Header = () => {
+    const [cookies] = useCookies();
+    const logout = () => {
+        AuthApi.signout(cookies.token);
+    };
     return (
         <div>
             <h1>갓생러헤더</h1>
+            <button onClick={logout}>로그아웃</button>
             <hr />
         </div>
     );
--- src/shared/api.js
+++ src/shared/api.js
@@ -17,6 +17,20 @@ export const AuthApi = {
         const response = await api.post('/login', payload);
         return response.data;
     },
+    signout: token => {
+        api.post('/logout', {
+            headers: {
+                'Content-Type': 'application/json', // 필요한 헤더를 여기에 추가하세요
+                Authorization: token, // 필요한 인증 헤더를 여기에 추가하세요
+            },
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
     checkEmailDuplication: async email => {
         const url = '/'; // 이메일 중복확인을 위한 끝점이 어딘지 설정해야함
         const response = await api.post(url, { email });
