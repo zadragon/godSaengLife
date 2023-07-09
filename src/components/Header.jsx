764940c150d2b02fc74a2dff526a43be7faf5787
zadragon <mase0804@naver.com>
2023-07-09 23:18:55 +0900

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect, useRef } from 'react';
-import { AuthApi } from '../shared/api';
+import { AuthApi, MainApi } from '../shared/api';
 import { useCookies } from 'react-cookie';
 import ReactCanvasConfetti from 'react-canvas-confetti';
 import { Link, useNavigate } from 'react-router-dom';
 import * as C from '../styles/common';
+import { useQuery } from '@tanstack/react-query';
 
 const Header = () => {
     const [cookies, removeCookie] = useCookies();
@@ -14,9 +15,14 @@ const Header = () => {
         navigate('/splash');
     };
 
+    const { data, isLoading, isError, isSuccess, refetch } = useQuery(['getMain'], () => MainApi.getProfile());
+    isLoading && '로딩중';
+    isError && '에러발생';
     return (
         <C.Header>
-            <p className="txtWelcome">환영합니다🎉 {cookies.Authorization ? '갓생러 님' : '방문자님 🤗'}</p>
+            <p className="txtWelcome">
+                환영합니다🎉 {cookies.Authorization ? `${data && data?.data.user.nickname}님 🤗` : '방문자님 🤗'}
+            </p>
             {cookies.Authorization ? (
                 <span className="signInOut" onClick={logout}>
                     로그아웃
